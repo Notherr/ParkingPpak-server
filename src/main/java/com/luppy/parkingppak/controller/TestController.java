2ab7defe835459043a9e43fd6c1db813d2ad71d6
seongkyu-lim <96x60812@naver.com>
2022-03-16 20:27:43 +0900

    add: springdoc (swagger)

--- src/main/java/com/luppy/parkingppak/controller/TestController.java
+++ src/main/java/com/luppy/parkingppak/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.luppy.parkingppak.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "test")
 public class TestController {
 
+    @Operation(summary = "테스트용 api입니다.")
     @GetMapping("/test")
     public String test(){
         return "test";
