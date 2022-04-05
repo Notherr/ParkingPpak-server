d4b94c01d9de474ba707b8771bd8658053f905ed
seongkyu-lim <96x60812@naver.com>
2022-04-05 17:59:16 +0900

    refactor: getAccountName, validateToken

--- src/main/java/com/luppy/parkingppak/utils/JwtUtil.java
+++ src/main/java/com/luppy/parkingppak/utils/JwtUtil.java
@@ -1,14 +1,17 @@
 package com.luppy.parkingppak.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+@Slf4j
 @Component
 public class JwtUtil {
 
@@ -36,14 +39,22 @@ public class JwtUtil {
 
     // 토큰에서 회원 정보 추출
     public String getAccountName(String jwt) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(jwt).getBody().getSubject();
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody().getSubject();
     }
 
     // 토큰의 유효성 + 만료일자 확인
     public boolean validateToken(String jwt) {
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(jwt);
-            return !claims.getBody().getExpiration().before(new Date());
+            // Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(jwt);
+            // return !claims.getBody().getExpiration().before(new Date());
+            Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt);
+            return true;
+        } catch (ExpiredJwtException e){
+            log.info("만료된 jwt 토큰입니다.");
+            return false;
         } catch (Exception e) {
             return false;
         }
