b860b5eed16100303f0e0481fba1de714f086fdf
seongkyu-lim <96x60812@naver.com>
2022-04-23 16:16:25 +0900

    add: account info in response body, when oauth login.

--- src/main/java/com/luppy/parkingppak/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/luppy/parkingppak/config/oauth/OAuth2SuccessHandler.java
@@ -1,6 +1,7 @@
 package com.luppy.parkingppak.config.oauth;
 
 import com.luppy.parkingppak.config.auth.AccountDetails;
+import com.luppy.parkingppak.domain.Account;
 import com.luppy.parkingppak.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -11,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @RequiredArgsConstructor
 @Component
@@ -19,12 +21,15 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final JwtUtil jwtUtil;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         AccountDetails accountDetails = (AccountDetails) authentication.getPrincipal();
-
+        Account account= accountDetails.getAccount();
         String jwtToken = jwtUtil.createToken(accountDetails.getAccount().getId(), accountDetails.getUsername());
 
         response.addHeader("Authorization", "Bearer "+jwtToken);
+
+        PrintWriter writer = response.getWriter();
+        writer.write(String.valueOf(account));
     }
 }
