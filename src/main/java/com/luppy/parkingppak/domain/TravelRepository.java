1759256bafcdb833abc801401453de898bec71bf
seongkyu-lim <96x60812@naver.com>
2022-04-30 15:52:29 +0900

    add: query method of TravelRepository for make data list in recommendSystem.

--- src/main/java/com/luppy/parkingppak/domain/TravelRepository.java
+++ src/main/java/com/luppy/parkingppak/domain/TravelRepository.java
@@ -3,6 +3,13 @@ package com.luppy.parkingppak.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface TravelRepository extends JpaRepository<Travel, Long> {
+    List<Travel> findAllByIdAndParkingLot(Long id, ParkingLot parkingLot);
+
+    List<Travel> findAllByIdAndGasStation(Long id, GasStation gasStation);
+
 }
