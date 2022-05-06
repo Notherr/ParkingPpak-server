774d6df32fb4304a8d0c2666e23899f4ea7156c4
seongkyu-lim <96x60812@naver.com>
2022-05-06 20:58:57 +0900

    add: schema about request dto.

--- src/main/java/com/luppy/parkingppak/domain/dto/AccountDto.java
+++ src/main/java/com/luppy/parkingppak/domain/dto/AccountDto.java
@@ -7,14 +7,22 @@ import lombok.Data;
 @Builder
 @Data
 public class AccountDto {
+
+    @Schema(hidden = true)
     private Long id;
+
     @Schema(example = "1234@google.com")
     private String email;
+
     @Schema(example = "1234")
     private String password;
+
     @Schema(example = "seongkyulim")
     private String name;
+
     // private String oilType;
     // private String card;
+
+    @Schema(hidden = true)
     private Boolean verified;
 }
