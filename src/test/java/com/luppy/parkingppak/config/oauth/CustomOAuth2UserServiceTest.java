721dc862d2cd7039135f28688f806b52c5944515
seongkyu-lim <96x60812@naver.com>
2022-05-06 20:14:22 +0900

    add: CustomOAuth2UserServiceTest, OAuth2SuccessHandlerTest.

---
+++ src/test/java/com/luppy/parkingppak/config/oauth/CustomOAuth2UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.luppy.parkingppak.config.oauth;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomOAuth2UserServiceTest {
+
+
+
+}
