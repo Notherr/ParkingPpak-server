62c0f0e206aedb10fc47642746539926f8ec0666
seongkyu-lim <96x60812@naver.com>
2022-05-06 11:02:05 +0900

    add: Response Dto.

---
+++ src/main/java/com/luppy/parkingppak/domain/dto/Response.java
@@ -0,0 +1,59 @@
+package com.luppy.parkingppak.domain.dto;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public class Response<T> {
+
+
+    private Integer statusCode;
+    private T data;
+    private String message;
+
+    //상황에 따른 message와 statusCode를 만들어주자.
+
+    public static <T> Response<T> JOIN_OK(T data){
+        return (Response<T>) Response.builder()
+                .statusCode(200)
+                .data(data)
+                .message("회원가입이 정상적으로 되었습니다.")
+                .build();
+    }
+
+    public static <T> Response<T> JOIN_ERROR(){
+        return (Response<T>) Response.builder()
+                .statusCode(400)
+                .data(null)
+                .message("이미 존재하는 이메일입니다.")
+                .build();
+    }
+
+    public static <T> Response<T> LOGIN_OK(T data){
+        return (Response<T>) Response.builder()
+                .statusCode(200)
+                .data(data)
+                .message("로그인이 정상적으로 되었습니다.")
+                .build();
+    }
+
+    public static <T> Response<T> NOT_JOIN_ERROR(){
+        return (Response<T>) Response.builder()
+                .statusCode(4000)
+                .data(null)
+                .message("가입되지 않은 이메일입니다.")
+                .build();
+    }
+
+    public static <T> Response<T> PASSWORD_ERROR(){
+        return (Response<T>) Response.builder()
+                .statusCode(4001)
+                .data(null)
+                .message("틀린 패스워드 입니다.")
+                .build();
+    }
+
+
+
+}
