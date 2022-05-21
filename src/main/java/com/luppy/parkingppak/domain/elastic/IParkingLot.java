d05170f15ad3e2662e8727e6567848209e5d55e3
hyungjungoo <9h0jun1115@gmail.com>
2022-05-22 01:16:39 +0900

    add elasticsearch config

---
+++ src/main/java/com/luppy/parkingppak/domain/elastic/IParkingLot.java
@@ -0,0 +1,49 @@
+package com.luppy.parkingppak.domain.elastic;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.time.LocalDateTime;
+
+@Document(indexName = "parking_lot")
+@Getter
+public class IParkingLot {
+
+    @Id
+    private Long id;
+
+    private String parkingName;
+
+    private String address;
+    // 고유 코드
+    private int parkingCode;
+    // 종류 e.g. 노외 주차장
+    private String type;
+    // 전화 번호
+    private String phoneNumber;
+    // 유무료 구분
+    private Boolean payYN;
+    // 주중 운영 시간
+    private String weekdayBegin;
+    private String weekdayEnd;
+    // 주말 운영 시간
+    private String weekendBegin;
+    private String weekendEnd;
+    // 공휴일 운영 시간
+    private String holidayBegin;
+    private String holidayEnd;
+    // 최종 데이터 동기화 시간
+    private LocalDateTime syncTime;
+    // 기본 주차 요금
+    private int rates;
+    // 기본 주차 시간 (분 단위)
+    private int timeRates;
+    // 추가 단위 요금
+    private int addRates;
+    // 추가 단위 시간 (분 단위)
+    private int addTimeRates;
+    // 주차장 위치 좌표
+    private double xCoor;
+    private double yCoor;
+}
