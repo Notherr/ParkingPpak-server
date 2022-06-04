593963e9601b0e458d524e5fa692f58c8532af24
seongkyu-lim <96x60812@naver.com>
2022-06-04 15:12:52 +0900

    add: RegisterCard Test.

--- src/test/java/com/luppy/parkingppak/controller/AccountControllerTest.java
+++ src/test/java/com/luppy/parkingppak/controller/AccountControllerTest.java
@@ -1,14 +1,44 @@
-/*
+
 package com.luppy.parkingppak.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luppy.parkingppak.domain.Account;
+import com.luppy.parkingppak.domain.AccountRepository;
+import com.luppy.parkingppak.utils.JwtUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class AccountControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Test
     public void 회원가입이_정상적으로_되었습니다 () {
@@ -34,6 +64,33 @@ public class AccountControllerTest {
     public void 틀린_패스워드_입니다 () {
 
     }
+
+    @Test
+    public void 카드정보가_정상적으로_등록되었습니다 () throws Exception {
+
+        Account account = Account.builder()
+                .email("test@gmail.com")
+                .password("1234")
+                .name("seongkyu")
+                .build();
+        Account savedAccount = accountRepository.save(account);
+
+        String jwt = "Bearer " + jwtUtil.createToken(savedAccount.getId(), savedAccount.getName());
+
+        Map<String, String> content = new HashMap<>();
+        content.put("name", "SINHAN ZERO CARD");
+        content.put("compName", "SINHAN");
+        content.put("content", "3%");
+
+        mockMvc.perform(put("/api/accounts/card-type")
+                        .header("Authorization", jwt)
+                        .content(objectMapper.writeValueAsString(content))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
 }
 
- */
