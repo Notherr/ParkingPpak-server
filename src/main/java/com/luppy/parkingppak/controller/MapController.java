6913b9ba7c3e20fd6a5a6fa756056ba469f7d568
seongkyu-lim <96x60812@gmail.com>
2022-06-25 17:00:11 +0900

    add: 주차장 데이터 리스트 조회 API.

--- src/main/java/com/luppy/parkingppak/controller/MapController.java
+++ src/main/java/com/luppy/parkingppak/controller/MapController.java
@@ -1,23 +1,28 @@
 package com.luppy.parkingppak.controller;
 
+import com.luppy.parkingppak.domain.ParkingLot;
+import com.luppy.parkingppak.domain.dto.MapRequestDto;
+import com.luppy.parkingppak.service.MapService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/map")
 public class MapController {
 
+    private final MapService mapService;
 
     @GetMapping("/")
-    public ResponseEntity<?> getParkingLotList(){
-
-        //좌표값을 받아서
-        return ResponseEntity.ok().body("");
+    public ResponseEntity<?> getParkingLotList(@RequestBody MapRequestDto dto){
+        /*
+         * 좌표를 받아서 해당 좌표의 범위내 데이터 반환.
+         */
+        List<ParkingLot> parkingLotList = mapService.getParkingLotList(dto);
+        return ResponseEntity.ok().body(parkingLotList);
     }
 
     @GetMapping("/{id}")
