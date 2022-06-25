c90442eae22673d0caabd1c3926acb453f23ce11
seongkyu-lim <96x60812@gmail.com>
2022-06-25 17:17:37 +0900

    refactor: getParkingLotList -> getDataList API.

--- src/main/java/com/luppy/parkingppak/controller/MapController.java
+++ src/main/java/com/luppy/parkingppak/controller/MapController.java
@@ -1,5 +1,6 @@
 package com.luppy.parkingppak.controller;
 
+import com.luppy.parkingppak.domain.GasStation;
 import com.luppy.parkingppak.domain.ParkingLot;
 import com.luppy.parkingppak.domain.dto.MapRequestDto;
 import com.luppy.parkingppak.domain.dto.ParkingLotDto;
@@ -18,16 +19,16 @@ public class MapController {
     private final MapService mapService;
 
     @GetMapping("/")
-    public ResponseEntity<?> getParkingLotList(@RequestBody MapRequestDto dto){
+    public ResponseEntity<?> getDataList(@RequestBody MapRequestDto dto){
         /*
          * 좌표를 받아서 해당 좌표의 범위내 데이터 반환.
          */
-        List<ParkingLot> parkingLotList = mapService.getParkingLotList(dto);
-        return ResponseEntity.ok().body(parkingLotList);
+            List<?> dataList = mapService.getDataList(dto);
+            return ResponseEntity.ok().body(dataList);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getParkingLot(@PathVariable String id){
+    public ResponseEntity<?> getData(@PathVariable String id){
 
         ParkingLot parkingLot  = mapService.getParkingLot(id);
         return ResponseEntity.ok().body(parkingLot);
