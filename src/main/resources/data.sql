62b9a33a8cc620ac56c9793a41251dcb4f0b790e
seongkyu-lim <96x60812@gmail.com>
2022-07-02 17:24:18 +0900

    refactor: data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,4 @@ insert into card(id, name, comp_name, content) values ('2', 'SAMSUNG BLACK', 'SA
 insert into card(id, name, comp_name, content) values ('3', 'HYUNDAI SMILE', 'HYUNDAI', '2%');
 insert into card(id, name, comp_name, content) values ('4', 'KB NICE CARD', 'KB', '3%');
 **/
-insert into card(id, card_name, comp_name, content) values ('2', 'SAMSUNG BLACK' 'SAMSUNG', '4%');
+insert into card(id, card_name, comp_name, content) values ('1','SAMSUNG BLACK', 'SAMSUNG', '4%');
