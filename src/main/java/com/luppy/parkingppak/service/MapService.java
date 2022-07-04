cd8528a2676f227839029d44cbe88e9908448f0d
seongkyu-lim <96x60812@gmail.com>
2022-07-04 22:45:34 +0900

    Revert "refactor: 주석."
    This reverts commit 60cc93d92c7a5018d290da627a87f55c351cb898.
    
    # Conflicts:
    #	src/main/java/com/luppy/parkingppak/service/MapService.java

---
+++ src/main/java/com/luppy/parkingppak/service/MapService.java
@@ -0,0 +1,36 @@
+package com.luppy.parkingppak.service;
+
+import com.luppy.parkingppak.domain.GasStationRepository;
+import com.luppy.parkingppak.domain.ParkingLotRepository;
+import com.luppy.parkingppak.domain.dto.MapRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class MapService {
+
+    private final ParkingLotRepository parkingLotRepository;
+    private final GasStationRepository gasStationRepository;
+
+    public List<?> getDataList(MapRequestDto dto){
+
+        // 요청받은 좌표값 기준 범위내 조회 쿼리메소드로 수정 필요.
+        // elasticsearch로 데이터 가져오는 것으로  수정 필요.
+        if(dto.getType().equals("parking-lot")) {
+            return parkingLotRepository.findAll();
+        }else{
+            return gasStationRepository.findAll();
+        }
+    }
+
+    public Object getData(MapRequestDto dto) {
+        if(dto.getType().equals("parking-lot")){
+            return parkingLotRepository.findById(dto.getId()).orElse(null);
+        }else{
+            return gasStationRepository.findById(dto.getId()).orElse(null);
+        }
+    }
+}
