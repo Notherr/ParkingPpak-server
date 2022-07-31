d1c7992fff0f6f444ecc1e79f25699da4fb37821
seongkyu-lim <96x60812@naver.com>
2022-07-31 21:34:53 +0900

    add: 이메일, 패스워드 검증 로직 추가.

--- src/main/java/com/luppy/parkingppak/controller/AccountController.java
+++ src/main/java/com/luppy/parkingppak/controller/AccountController.java
@@ -8,6 +8,7 @@ import com.luppy.parkingppak.domain.enumclass.NaviType;
 import com.luppy.parkingppak.domain.enumclass.OilType;
 import com.luppy.parkingppak.service.AccountService;
 import com.luppy.parkingppak.utils.JwtUtil;
+import com.luppy.parkingppak.utils.ValidationUtil;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -31,14 +32,13 @@ public class AccountController {
     private final AccountRepository accountRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final JwtUtil jwtUtil;
+    private final ValidationUtil validationUtil;
 
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody AccountDto dto) {
-        String regx = "^(.+)@(.+)$";
-        Pattern pattern = Pattern.compile(regx);
-        Matcher matcher = pattern.matcher(dto.getEmail());
 
-        if (!matcher.matches()) return ResponseEntity.badRequest().body(Response.INVALID_EMAIL_ERROR());
+        if (!validationUtil.validate_email(dto.getEmail())) return ResponseEntity.badRequest().body(Response.INVALID_EMAIL_ERROR());
+        if (!validationUtil.validate_password(dto.getPassword())) return ResponseEntity.badRequest().body(Response.INVALID_PASSWORD_ERROR());
 
         AccountDto registeredAccount = accountService.joinAccount(dto);
 
