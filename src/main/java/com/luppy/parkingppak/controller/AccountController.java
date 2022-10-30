f5b726a8ab2bdb6edd2df8e73d8a001cd2503dbd
seongkyulim <96x60812@gmail.com>
2022-10-30 16:08:48 +0900

    Refactor: Accounts login 로직 주석 제거

--- src/main/java/com/luppy/parkingppak/controller/AccountController.java
+++ src/main/java/com/luppy/parkingppak/controller/AccountController.java
@@ -1,27 +1,16 @@
 package com.luppy.parkingppak.controller;
 
-import com.luppy.parkingppak.domain.Account;
-import com.luppy.parkingppak.domain.AccountRepository;
-import com.luppy.parkingppak.domain.Card;
+
 import com.luppy.parkingppak.domain.dto.*;
 import com.luppy.parkingppak.domain.enumclass.NaviType;
 import com.luppy.parkingppak.domain.enumclass.OilType;
 import com.luppy.parkingppak.service.AccountService;
-import com.luppy.parkingppak.utils.JwtUtil;
 import com.luppy.parkingppak.utils.ValidationUtil;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -29,9 +18,6 @@ import java.util.regex.Pattern;
 public class AccountController {
 
     private final AccountService accountService;
-    private final AccountRepository accountRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final JwtUtil jwtUtil;
     private final ValidationUtil validationUtil;
 
     @PostMapping("/join")
@@ -45,32 +31,6 @@ public class AccountController {
         if(registeredAccount == null) return ResponseEntity.badRequest().body(Response.JOIN_ERROR());
         else return ResponseEntity.ok().body(Response.JOIN_OK(registeredAccount));
     }
-
-//    @PostMapping("/login")
-//    public ResponseEntity<?> login(@RequestBody LoginRequestDto dto) {
-//
-//        Optional<Account> account = accountRepository.findByEmail(dto.getEmail());
-//
-//        if (account.isEmpty()) return ResponseEntity.status(400).body(Response.NOT_JOIN_ERROR());
-//        else {
-//            if (!bCryptPasswordEncoder.matches(dto.getPassword(), account.get().getPassword())) {
-//                return ResponseEntity.status(401).body(Response.PASSWORD_ERROR());
-//            }
-//
-//            String jwtToken = "Bearer " + jwtUtil.createToken(account.get().getId(), account.get().getName());
-//
-//            LoginResponseDto loginResponseDto = LoginResponseDto.builder()
-//                    .email(account.get().getEmail())
-//                    .name(account.get().getName())
-//                    .jwt(jwtToken)
-//                    .card(account.get().getCard())
-//                    .oilType(account.get().getOilType())
-//                    .naviType(account.get().getNaviType())
-//                    .build();
-//
-//            return ResponseEntity.ok().body(Response.LOGIN_OK(loginResponseDto));
-//        }
-//    }
 
     @PutMapping("/accounts/card-type")
     public ResponseEntity<?> registerCard(@RequestHeader("Authorization") String jwt, @RequestBody CardDto cardDto) {
