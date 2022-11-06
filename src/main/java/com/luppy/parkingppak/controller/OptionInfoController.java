630eab5d062704f096978f38cb2e7d9b198703d1
seongkyulim <96x60812@gmail.com>
2022-11-06 14:21:11 +0900

    Refactor: 옵션(카드정보, 유류정보, 내비정보) 리스트 반환 로직

--- src/main/java/com/luppy/parkingppak/controller/OptionInfoController.java
+++ src/main/java/com/luppy/parkingppak/controller/OptionInfoController.java
@@ -1,6 +1,5 @@
 package com.luppy.parkingppak.controller;
 
-import com.luppy.parkingppak.domain.dto.Response;
 import com.luppy.parkingppak.domain.enumclass.CardCompName;
 import com.luppy.parkingppak.domain.enumclass.NaviType;
 import com.luppy.parkingppak.domain.enumclass.OilType;
@@ -9,8 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.luppy.parkingppak.domain.dto.Response.response;
 
@@ -21,35 +21,23 @@ public class OptionInfoController {
     @GetMapping("/card-comp-name")
     public ResponseEntity<?> getCardCompNameList(){
 
-        List<CardCompName> cardCompNameList = new ArrayList<>();
-        cardCompNameList.add(CardCompName.HYUNDAI);
-        cardCompNameList.add(CardCompName.KB);
-        cardCompNameList.add(CardCompName.SAMSUNG);
-        cardCompNameList.add(CardCompName.SINHAN);
+        List<String> cardCompNameList = Stream.of(CardCompName.values()).map(Enum::name).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(response(200, cardCompNameList, "정상적으로 카드 회사 이름 목록이 반환되었습니다." ));
     }
 
-    @GetMapping("/oil-type")
+   @GetMapping("/oil-type")
     public ResponseEntity<?> getOilTypeList(){
-        List<OilType> oilTypeList = new ArrayList<>();
-        oilTypeList.add(OilType.GASOLINE);
-        oilTypeList.add(OilType.VIA);
-        oilTypeList.add(OilType.ELECTRIC);
-        oilTypeList.add(OilType.PREMIUM);
-        oilTypeList.add(OilType.LPG);
+
+        List<String> oilTypeList = Stream.of(OilType.values()).map(Enum::name).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(response(200, oilTypeList, "정상적으로 유류 타입 목록이 반환되었습니다."));
     }
 
     @GetMapping("/navi-type")
     public ResponseEntity<?> getNaviTypeList(){
-        List<NaviType> naviTypeList = new ArrayList<>();
-        naviTypeList.add(NaviType.GOOGLE);
-        naviTypeList.add(NaviType.KAKAOMAP);
-        naviTypeList.add(NaviType.KAKAONAVI);
-        naviTypeList.add(NaviType.NAVER);
-        naviTypeList.add(NaviType.TMAP);
+
+        List<String> naviTypeList = Stream.of(NaviType.values()).map(Enum::name).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(response(200, naviTypeList, "정상적으로 내비 앱 목록이 반환되었습니다."));
     }
